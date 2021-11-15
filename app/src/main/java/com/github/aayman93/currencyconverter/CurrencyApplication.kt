34820a4092d8c5951777afb07055cfd5bf31b1d5
aayman93 <ahmedaymann93@gmail.com>
2021-11-15 16:17:54 +0200

    Layout

--- app/src/main/java/com/github/aayman93/currencyconverter/CurrencyApplication.kt
+++ app/src/main/java/com/github/aayman93/currencyconverter/CurrencyApplication.kt
@@ -1,4 +1,7 @@
 package com.github.aayman93.currencyconverter
 
-class CurrencyApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication : Application()
